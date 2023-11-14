af95989e756dd62951d60502b9ba83a9df6de6ec
Maxim Kolomeychenko <maxim@supervise.ly>
2023-11-14 16:21:47 +0000

    add SLY SDK and ignore symlink

--- src/main.py
+++ src/main.py
@@ -1,15 +1,20 @@
 import supervisely as sly
 
-from supervisely.app.widgets import Container
+# from supervisely.app.widgets import Container
 
-import src.globals as g
-import src.ui.input as input
-import src.ui.settings as settings
-import src.ui.output as output
+# import src.globals as g
+# import src.ui.input as input
+# import src.ui.settings as settings
+# import src.ui.output as output
 
-layout = Container(widgets=[input.card, settings.card, output.card])
+# layout = Container(widgets=[input.card, settings.card, output.card])
 
-# * If the app uses static dir, it should be passed as a parameter.
-# * If not needed the app can be initialized without static_dir parameter.
-# * app = sly.Application(layout=layout)
-app = sly.Application(layout=layout, static_dir=g.STATIC_DIR)
+# # * If the app uses static dir, it should be passed as a parameter.
+# # * If not needed the app can be initialized without static_dir parameter.
+# # * app = sly.Application(layout=layout)
+# app = sly.Application(layout=layout, static_dir=g.STATIC_DIR)
+
+
+from supervisely.app.widgets import Button, Text, Container
+
+button = Button("change text")
