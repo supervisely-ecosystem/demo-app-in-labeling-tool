d5919e888db083ab0becb4fb6b14151f96dc0610
Maxim Kolomeychenko <maxim@supervise.ly>
2023-11-15 08:55:43 +0000

    wip

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import supervisely as sly
 
 from supervisely.app.widgets import Button, Text, Container
 
-# load_dotenv(os.path.expanduser("~/supervisely.env"))
+load_dotenv(os.path.expanduser("~/supervisely.env"))
 
 button = Button("random text")
 text = Text("press the button to get random text")
